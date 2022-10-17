1e1b41155ac4b75775493940327ea9071e2dce0a
Youngwook-Go <Youngwook-Go@users.noreply.github.com>
2022-10-17 02:28:44 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@
 // Created by: Youngwook
 // Created on: Oct 2022
 // This file contains the JS functions for index1.html
-'use strict'
+"use strict"
 /**
  * This function gets users area and permiter of triangle.
  */
 
-function submit () {
+function submit() {
   // input
-  const base = parseInt(document.getElementById('base-value').value)
-  const height = parseInt(document.getElementById('height-value').value)
+  const base = parseInt(document.getElementById("base-value").value)
+  const height = parseInt(document.getElementById("height-value").value)
 
   // process
-  const area = ((base * height) / 2)
+  const area = (base * height) / 2
 
   // output
-  document.getElementById('area').innerHTML = ' The area is : ' + area + ' ㎠ '
+  document.getElementById("area").innerHTML = " The area is : " + area + " ㎠ "
 }
